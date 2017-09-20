a37f599e59f293e8c74de37dbf5e5d83aace3ead
blade <blade@DESKTOP-7HJ2ACS>
2017-09-20 21:36:42 +0300

    Registration. DbContext. Migration

---
+++ Blog/Blog.DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<Entities.ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Blog/Blog.Gateway/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog.Gateway.Startup))]
+namespace Blog.Gateway
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
